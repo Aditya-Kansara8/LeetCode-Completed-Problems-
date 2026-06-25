420dea1e153e5c1640f5353083ca249fd56eded4
Aditya Pranav Kansara <106244794+Aditya-Kansara8@users.noreply.github.com>
2026-06-24 19:56:50 -0500

    Time: 132 ms (42.95%), Space: 31.9 MB (69.43%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,14 +6,10 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        head = root.val
-        count = 1
         def good(node,head):
             if node is None:
                 return 0
-            if node.val >= head:
-                return 1 + good(node.left, node.val) + good(node.right,node.val)
-            else:
-                return good(node.left, head) + good(node.right,head) 
+            head = max(head, node.val)
+            return (head == node.val) + good(node.left, head) + good(node.right,head) 
         return good(root,root.val)
         
